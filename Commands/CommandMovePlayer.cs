9cbcd2bc764c7c33961e8a5ba4600d42fafebb69
John Xuan <rpgmage48@gmail.com>
2025-09-29 14:42:11 -0400

    added in a command for moving the player

---
+++ Commands/CommandMovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    public class CommandMovePlayer : ICommand
+    {
+        private Player currentPlayer;
+        private Cardinal inputDirection;
+
+        public CommandMovePlayer(Player player, Cardinal direction) 
+        {
+            currentPlayer = player;
+            inputDirection = direction;
+        }
+
+        public void Execute()
+        {
+            currentPlayer.Move(inputDirection);
+        }
+
+    }
+}
