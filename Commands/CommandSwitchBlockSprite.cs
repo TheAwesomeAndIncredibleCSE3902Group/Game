d70d1d4a22f4ecbba78a7c49047675127493041f
iris122osu <moorman.122@osu.edu>
2025-09-29 13:56:02 -0400

    #8 implemented switchblocksprite command

--- Commands/CommandSwitchBlockSprite.cs
+++ Commands/CommandSwitchBlockSprite.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sprint0.Tiles;
 
 namespace Sprint0.Commands
 {
     public class CommandSwitchBlockSprite : ICommand
     {
 
-        public CommandSwitchBlockSprite() 
+        private int Column;
+        private int Row;
+        private int Id; 
+        private Tilemap Tilemap;
+
+        public CommandSwitchBlockSprite(Tilemap tilemap, int tile_id, int column, int row)
         {
+            Tilemap = tilemap;
+            Column = column;
+            Row = row;
+            Id = tile_id;
         }
 
         public void Execute()
         {
-
+            Tilemap.SetTile(Column, Row, Id);
         }
 
     }
