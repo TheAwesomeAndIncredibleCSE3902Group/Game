0fff2bb285662f13b2195156ae9c1e43e0c798c4
iris122osu <moorman.122@osu.edu>
2025-10-02 20:03:43 -0400

    #6 implemented Command for switching enemy

--- Commands/CommandSwitchEnemySprite.cs
+++ Commands/CommandSwitchEnemySprite.cs
@@ -1,22 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Sprint0.Characters;
 
 namespace Sprint0.Commands
 {
     public class CommandSwitchEnemySprite : ICommand
     {
         private Game1 myGame;
+        private int direction = 0;
 
-        public CommandSwitchEnemySprite() 
+        public CommandSwitchEnemySprite(Game1 game, bool right)
         {
+            myGame = game;
+            direction += right ? -1 : 1;
         }
 
         public void Execute()
         {
-
+            int Size = myGame._characterSet.Count;
+            if (direction + myGame.currentEnemy >= Size)
+            {
+                myGame.currentEnemy = 0;
+            }
+            else if (direction + myGame.currentEnemy < 0)
+            {
+                myGame.currentEnemy = Size;
+            }
+            else
+            {
+                myGame.currentEnemy += direction;
+            }
+            
         }
 
     }
