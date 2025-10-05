01a52be4653e7f997e80af07a36d2061f39d1915
John Xuan <rpgmage48@gmail.com>
2025-10-05 11:47:57 -0400

    Added in the reset command

---
+++ Commands/CommandResetGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Characters;
+using Sprint0.Sprites;
+
+namespace Sprint0.Commands
+{
+    internal class CommandResetGame : ICommand
+    {
+        private Game1 myGame;
+        public CommandResetGame(Game1 game) { 
+            myGame = game;
+        }
+
+        public void Execute() {
+            myGame._characterSet.Clear();
+            myGame._characterSet.Add(new CharacterEnemyMoblin(new Vector2(300, 350), Util.Cardinal.up));
+            myGame._characterSet.Add(new CharacterEnemyArmos(new Vector2(300, 350), Util.Cardinal.down));
+            myGame._characterSet.Add(new CharacterEnemyLynel(new Vector2(300, 350), Util.Cardinal.right));
+            myGame._characterSet.Add(new CharacterKris());
+            myGame.ChangeGameSpriteToNewSprite("item", MapItemSpriteFactory.CreatePotionSprite());
+            myGame.Tilemap.SetTile(1, 2, 9);
+            myGame.Player.Position = new Vector2(300, 300);
+            myGame.currentEnemy = 0;
+        }
+    }
+}
