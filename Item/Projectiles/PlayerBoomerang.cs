07d3e293f5ef20c72f9217e087775fc122c1f151
rosenglass <lambstutz@protonmail.com>
2025-10-11 21:36:42 -0400

    #51 Boomerang now returns to Link

--- Item/Projectiles/PlayerBoomerang.cs
+++ Item/Projectiles/PlayerBoomerang.cs
@@ -15,7 +15,7 @@ public class PlayerBoomerang : Projectile
     float initialSpeed;
     public PlayerBoomerang(Vector2 position, Cardinal direction)
     {
-        
+
         this.position = position;
         this.direction = direction;
         initialSpeed = 2;
@@ -28,10 +28,36 @@ public class PlayerBoomerang : Projectile
         sprite = ItemSpriteFactory.CreateBoomerangSprite();
     }
 
+/*
     protected override void Move()
     {
         base.Move();
         movementSpeed = initialSpeed - (age / lifetime) * 2 * initialSpeed; //Silly math to make the boomerang go back
+    }
+    */
+
+    protected override void Move()
+    {
+        if (age < lifetime / 2)
+            MoveForward();
+        else
+            MoveBack();
+    }
+
+    private void MoveForward()
+    {
+        base.Move();
+    }
+    
+    /// <summary>
+    /// Moves back a bit faster so it is basically guaranteed to get back to the player
+    /// </summary>
+    private void MoveBack()
+    {
+
+        Vector2 toPlayer = Player.Instance.Position - position;
+        toPlayer.Normalize();
+        position += 2 * movementSpeed * toPlayer;
     }
 
     public override void Destroy()
