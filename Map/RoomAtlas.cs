11c2d434a6b7307cd2e895a10bd9aee0bdea2994
John Xuan <rpgmage48@gmail.com>
2025-10-24 15:41:21 -0400

    added more functionality in RoomAtlas

--- Map/RoomAtlas.cs
+++ Map/RoomAtlas.cs
@@ -13,6 +13,42 @@ namespace AwesomeRPG.Map
             atlas[0].Add(startingRoom);
         }
 
+        public RoomAtlas(List<List<RoomMap>> startingAtlas)
+        {
+            atlas = startingAtlas;
+        }
+
+        public RoomMap GetRoom(int column, int row)
+        {
+            return atlas[column][row];
+        }
+
+        public int GetColumn(RoomMap room)
+        {
+            for (int i = 0; i < atlas.Count; i++)
+            {
+                if (atlas[i].Contains(room))
+                {
+                    return i;
+                }
+            }
+            //room not found
+            return -1;
+        }
+
+        public int GetRow(RoomMap room)
+        {
+            for (int i = 0; i < atlas.Count; i++)
+            {
+                if (atlas[i].Contains(room))
+                {
+                    return atlas[i].IndexOf(room);
+                }
+            }
+            //room not found
+            return -1;
+        }
+
         public void AddRoom(RoomMap room, int column, bool horizontal, bool increase)
         {
             if (atlas[column].Contains(room))
@@ -49,9 +85,41 @@ namespace AwesomeRPG.Map
             }
         }
 
-        public RoomMap GetRoom(int column, int row)
+        public void RemoveRoom(RoomMap room)
         {
-            return atlas[column][row];
+            for (int i = 0; i < atlas.Count; i++)
+            {
+                if (atlas[i].Contains(room))
+                {
+                    int roomIndex = atlas[i].IndexOf(room);
+                    atlas[i].Remove(room);
+                    // Optionally, remove the column if it's empty
+                    if (atlas[i].Count == 0)
+                    {
+                        atlas.RemoveAt(i);
+                    }
+                    // Creates an empty RoomMap placeholder to maintain row position.
+                    else
+                    {
+                        atlas[i].Insert(roomIndex, new RoomMap(null));
+                    }
+                    return;
+                }
+            }
+        }
+
+        public void ReplaceRoom(RoomMap oldRoom, RoomMap newRoom)
+        {
+            for (int i = 0; i < atlas.Count; i++)
+            {
+                if (atlas[i].Contains(oldRoom))
+                {
+                    int roomIndex = atlas[i].IndexOf(oldRoom);
+                    atlas[i].Remove(oldRoom);
+                    atlas[i].Insert(roomIndex, newRoom);
+                    return;
+                }
+            }
         }
     }
 }
