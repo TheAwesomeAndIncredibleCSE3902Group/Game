848eea267b1190edc09d5cd8b8926471823c8336
John Xuan <rpgmage48@gmail.com>
2025-10-24 14:40:59 -0400

    Created a RoomAtlas to track the positions of rooms in relation to each other

---
+++ Map/RoomAtlas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeRPG.Map
+{
+    public class RoomAtlas
+    {
+        private List<List<RoomMap>> atlas;
+        public RoomAtlas(RoomMap startingRoom) {
+            atlas[0].Add(startingRoom);
+        }
+
+        public void AddRoom(RoomMap room, int column, bool horizontal, bool increase)
+        {
+            if (atlas[column].Contains(room))
+            {
+                //early exit if room already exists in atlas
+                return;
+            }
+
+            if (horizontal)
+            {
+                if (increase)
+                {
+                    // add a new column to the right
+                    atlas.Add(new List<RoomMap> { room });
+                }
+                else
+                {
+                    // add a new column to the left
+                    atlas.Insert(0, new List<RoomMap> { room });
+                }
+            }
+            else
+            {
+                if (increase)
+                {
+                    //add a new row to the bottom of the specified column
+                    atlas[column].Add(room);
+                }
+                else
+                {
+                    //add a new row to the top of the specified column
+                    atlas[column].Insert(0, room);
+                }
+            }
+        }
+
+        public RoomMap GetRoom(int column, int row)
+        {
+            return atlas[column][row];
+        }
+    }
+}
