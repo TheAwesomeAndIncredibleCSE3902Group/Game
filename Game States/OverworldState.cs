351683bd8a021f088278fae12e875a5bf8ad54d8
csandcastle <csandcastle119@gmail.com>
2025-12-10 14:11:48 -0500

    #76 Readded PlayerHud
    ### OverworldState
    Now makes use of new UI system. Tracks the health of all party members.
    
    ### Content
    Added new font called ZeldaFont which fits with the game's vibe more.

--- Game States/OverworldState.cs
+++ Game States/OverworldState.cs
@@ -8,6 +8,8 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using AwesomeRPG.UI;
 using System.Diagnostics;
+using AwesomeRPG.UI.ElementFactories;
+//using System.Drawing;
 
 namespace AwesomeRPG;
 
@@ -101,19 +103,29 @@ public class OverworldState : IGameState
     {
         RootElement rootUIElement = new RootElement(spriteBatch);
 
+        
         //Ensure the font is loaded
-        var spriteFont = game.Content.Load<SpriteFont>("Fonts\\MyFont");
+        var spriteFont = game.Content.Load<SpriteFont>("Fonts\\ZeldaFont");
+        TextElementFactory textElementFactory = new TextElementFactory(rootUIElement);
+        string healthString = GetPartyHealthString();
+        Element textElem = textElementFactory.CreateNew(spriteFont, 
+                                                        healthString, 
+                                                        Color.White,
+                                                        TextElementFactory.TextAlign.Left,
+                                                        TextElementFactory.TextAlign.Right);
+        textElem.OffsetAndSize = Util.ScreenRect;
+        rootUIElement.AddChild(textElem);
+         rootUIElement.Draw(gameTime);
+    }
 
-        // //Ensure the font is loaded
-        // var spriteFont = game.Content.Load<SpriteFont>("Fonts\\MyFont");
-
-        // //Text element construction
-        // String textString = Player.Instance.PlayerStats[0].GetHealth().ToString();
-        // TextElement textElem = new TextElement(rootUIElement, spriteFont, textString, Color.White);
-        // textElem.OffsetAndSize = game.GetScreenRect();
-        // textElem.HorizontalTextAlign = TextElement.TextAlign.Left;
-        // textElem.VerticalTextAlign = TextElement.TextAlign.Right;
-        // rootUIElement.AddChild(textElem);
-        // rootUIElement.Draw(gameTime);
+    private string GetPartyHealthString()
+    {
+        string healthString = "";
+        var partyHealths = Player.Instance.Party;
+        foreach(var member in partyHealths)
+        {
+            healthString += $"{member.Type}: {member.GetHealth()}HP\n";
+        }
+        return healthString;
     }
 }
